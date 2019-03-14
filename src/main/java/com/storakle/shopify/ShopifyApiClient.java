8fa533b42af130a03020264d4f570b75d327e568
Jack Walker <jack@foresightmobile.com>
2019-03-14 16:12:38 +0000

    Modified the getOrders method on the APIClient class.

--- src/main/java/com/storakle/shopify/ShopifyApiClient.java
+++ src/main/java/com/storakle/shopify/ShopifyApiClient.java
@@ -38,8 +38,8 @@ public interface ShopifyApiClient
     @RequestLine("GET /admin/collects/count.json")
     Count getCollectsCount();
 
-    @RequestLine("GET /admin/orders.json?limit={limit}&since_id={since-id}&page={page}&fields={fields}&status=any")
-    OrderList getOrders(@Param("limit") Integer limit, @Param("since-id") String sinceId, @Param("page") Integer page, @Param("fields") String fields);
+    @RequestLine("GET /admin/orders.json?limit={limit}&page={page}&fields={fields}&status=any&updated_at_min={updateAtMin}")
+    OrderList getOrders(@Param("limit") Integer limit, @Param("page") Integer page, @Param("fields") String fields, @Param("updateAtMin") String updatedAfter);
 
     @RequestLine("GET /admin/orders/count.json?status=any")
     Count getOrdersCount();
